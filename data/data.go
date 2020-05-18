1c2ac837dc8b9dd49f83340272334a61f98d3873
huangtao-sh <huangtao.jh@gmail.com>
2020-05-19 07:10:53 +0800

    fix bugs

--- data/data.go
+++ data/data.go
@@ -1,17 +1,36 @@
 package data
 
-import "sync"
+import (
+	"fmt"
+	"grape/sqlite3"
+	"sync"
+)
 
 // Data 数据传输
 type Data struct {
-	sync.WaitGroup                    // 进程控制
-	ch             chan []interface{} // 数据通道
+	wg *sync.WaitGroup    // 进程控制
+	ch chan []interface{} // 数据通道
 }
 
 // NewData 构造函数
 func NewData() *Data {
 	ch := make(chan []interface{})
-	return &Data{ch: ch}
+	return &Data{&sync.WaitGroup{}, ch}
+}
+
+// Done 执行完成
+func (d *Data) Done() {
+	d.wg.Done()
+}
+
+// Add 增加计数
+func (d *Data) Add(count int) {
+	d.wg.Add(count)
+}
+
+// Wait 阻塞进程，等待协程运行结束
+func (d *Data) Wait() {
+	d.wg.Wait()
 }
 
 // Close 关闭数据通道
@@ -27,4 +46,42 @@ func (d *Data) ReadCh() <-chan []interface{} {
 // WriteCh 读取数据通道
 func (d *Data) WriteCh() chan<- []interface{} {
 	return d.ch
+}
+
+// Write 向通道中写入数据
+func (d *Data) Write(data ...interface{}) {
+	d.ch <- data
+}
+
+// Println 打印通道中的数据
+func (d *Data) Println() {
+	defer d.Done()
+	for row := range d.ch {
+		fmt.Println(row...)
+	}
+}
+
+// Printf 打印通道中的数据
+func (d *Data) Printf(format string) {
+	defer d.Done()
+	for row := range d.ch {
+		fmt.Printf(format, row...)
+	}
+}
+
+// Exec 执行 SQL 语句
+func (d *Data) Exec(tx *sqlite3.Tx, sql string) (err error) {
+	defer d.Done()
+	stmt, err := tx.Prepare(sql)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	for row := range d.ReadCh() {
+		_, err = stmt.Exec(row...)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
--- sqlite3/tx.go
+++ sqlite3/tx.go
@@ -2,7 +2,6 @@ package sqlite3
 
 import (
 	"database/sql"
-	"grape/data"
 	"grape/util"
 	"unsafe"
 )
@@ -17,18 +16,6 @@ func NewTx() *Tx {
 	tx, err := NewDB().Begin()
 	util.CheckFatal(err)
 	return (*Tx)(unsafe.Pointer(tx))
-}
-
-// ExecCh 批量执行 sql 语句，数据从通道中获取
-func (tx *Tx) ExecCh(sql string, d *data.Data) {
-	defer d.Done()
-	stmt, err := tx.Prepare(sql)
-	util.CheckFatal(err)
-	defer stmt.Close()
-	for row := range d.ReadCh() {
-		_, err = stmt.Exec(row...)
-		util.CheckFatal(err)
-	}
 }
 
 // FetchValue 查询值
