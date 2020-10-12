5b118111abb7fd47ea0b3e860cc900345f8c6064
huangtao-sh <huangtao.jh@gmail.com>
2020-10-12 17:16:11 +0800

    upgrade

--- test/test.go
+++ test/test.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 )
 
-func yz(num int) (result []int, err error) {
+func getPrimes(num int) (primes []int, err error) {
 	if num <= 1 {
 		err = errors.New("整数不应小于1")
 	} else {
 		prime := 2
 		for num > 1 {
 			if num%prime == 0 {
-				result = append(result, prime)
+				primes = append(primes, prime)
 				num /= prime
 			} else {
 				prime++
@@ -25,7 +25,7 @@ func main() {
 	var i int
 	fmt.Printf("Please enter a number:")
 	fmt.Scanf("%d", &i)
-	k, err := yz(i)
+	k, err := getPrimes(i)
 	if err == nil {
 		fmt.Println(k)
 	}
