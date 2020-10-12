9e35214e7009cab0b348e88196b77700cfad68c3
huangtao-sh <huangtao.jh@gmail.com>
2020-10-12 17:18:32 +0800

    upgrade

--- test/test.go
+++ test/test.go
@@ -8,15 +8,15 @@ import (
 func getPrimes(num int) (primes []int, err error) {
 	if num <= 1 {
 		err = errors.New("整数不应小于1")
-	} else {
-		prime := 2
-		for num > 1 {
-			if num%prime == 0 {
-				primes = append(primes, prime)
-				num /= prime
-			} else {
-				prime++
-			}
+		return
+	}
+	prime := 2
+	for num > 1 {
+		if num%prime == 0 {
+			primes = append(primes, prime)
+			num /= prime
+		} else {
+			prime++
 		}
 	}
 	return
