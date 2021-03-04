a5c98676e6d395e3b3e30ef53d2cf963d2aab007
huangtao-sh <huangtao.jh@gmail.com>
2021-03-04 18:28:58 +0800

    upgrade

--- rhzh/query.go
+++ rhzh/query.go
@@ -11,11 +11,14 @@ where b.zh is null
 order by a.zh`
 	bszq = `select a.zh,a.yshm,b.hm,a.zhlb,b.zhxz,a.khrq,b.khrq,a.xhrq,b.xhrq,a.zt,b.zt 
 from bhsj a left join rhsj b on a.zh=b.zh or "NRA"||a.zh=b.zh 
-where b.zh is null or a.hm<>b.hm or a.khrq<>b.khrq or a.xhrq<>b.xhrq or a.zt<>b.zt or a.zhlb<>b.zhlb `
+where b.zh is null or a.hm<>b.hm or a.khrq<>b.khrq or a.xhrq<>b.xhrq or a.zt<>b.zt or a.zhlb<>b.zhlb`
+	cftj = `select a.zh,count(a.zh),sum(case when a.hm=b.hm and a.khrq=b.khrq and a.xhrq=b.xhrq and a.zt=b.zt and a.zhlb=b.zhlb then 1 else 0 end)
+from bhsj a left join rhsj b on a.zh=b.zh or "NRA"||a.zh=b.zh 
+group by a.zh having count(a.zh)>1`
 )
 
 // Query1 test
 func Query1() {
 	//sqlite3.Println(bszq)
-	sqlite3.Println(bszq)
+	sqlite3.Println(cftj)
 }
