b8d04df7eaf9020e08b5b47e0f1135b6e53aa04c
huangtao-sh <huangtao.jh@gmail.com>
2021-04-21 21:45:24 +0800

    jy fix bugs

--- params/jym/main.go
+++ params/jym/main.go
@@ -55,8 +55,9 @@ func Main() {
 					sqlite3.Printf("待投产  ：%4s  %-40s\n", "select jym,jymc from jymcs where jym=?", arg)
 					sqlite3.Printf("交易菜单：%4s  %-40s\n", "select jym,name from menu where jym=?", arg)
 				} else {
-					err := sqlite3.PrintRow(header+",创建日期,投产日期", "select * from jycs where jym=?", arg)
+					err := sqlite3.PrintRow(header ,"select * from jycs where jym=?", arg)
 					if err != nil {
+						fmt.Println(err)
 						fmt.Printf("错误：交易码 %s 不存在\n", arg)
 					}
 				}
